79317f836300b6c6cbb8bde7d0ba0a13f2f7756f
jajdsadlsldaldasdlasd <gono.cfg@gmail.com>
2026-09-25 17:22:36 +0500

    Update 3.py

--- tasks /3.py
+++ tasks /3.py
@@ -7,10 +7,12 @@ spec_symbols = "!@#$%^&*"
 alph = string.printable[36:62]
 nums = '0123456789'
 
-password = ""
+password_chars = []
+password_chars += random.choices(spec_symbols, k=2)
+password_chars += random.choices(alph, k=3)
+password_chars += random.choices(nums, k=3)
+random.shuffle(password_chars)
 
-password += ''.join(random.choices(spec_symbols, k=2))
-password += ''.join(random.choices(alph, k=3))
-password += ''.join(random.choices(nums, k=3))
+password = ''.join(password_chars)
 
 print(password)

d9fb0c928da5c123c2732cfff59e69395b6375b1
jajdsadlsldaldasdlasd <gono.cfg@gmail.com>
2026-09-25 18:16:36 +0500

    Update 3.py

--- tasks /3.py
+++ tasks /3.py
@@ -4,13 +4,13 @@ import random
 import string
 
 spec_symbols = "!@#$%^&*"
-alph = string.printable[36:62]
-nums = '0123456789'
+alphabet = string.printable[36:62]
+numbers = '0123456789'
 
 password_chars = []
 password_chars += random.choices(spec_symbols, k=2)
-password_chars += random.choices(alph, k=3)
-password_chars += random.choices(nums, k=3)
+password_chars += random.choices(alphabet, k=3)
+password_chars += random.choices(numbers, k=3)
 random.shuffle(password_chars)
 
 password = ''.join(password_chars)
